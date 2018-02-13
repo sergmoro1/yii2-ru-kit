efd6eb0c405a3dbc4624f287cb63df09366c9ed1
Sergey Morozov <sergmoro1@ya.ru>
2018-02-13 18:03:31 +0300

    calculate month number once

--- src/RuDate.php
+++ src/RuDate.php
@@ -12,14 +12,13 @@
  *  ];
  * }
  */
-namespace sergmoro1\blog\components;
+namespace sergmoro1\rudate;
 
-use Yii;
 use yii\base\Behavior;
 
 class RuDate extends Behavior
 {
-	private static $default = ['ru-RU' => 'd M Y', 'en-US' => 'Y M d'];
+    private static $default = ['ru-RU' => 'd M Y', 'en-US' => 'Y M d'];
     // Russian names of months
     private static $month = ['01'=>'Январь', '02'=>'Февраль', '03'=>'Март', '04'=>'Апрель', '05'=>'Май', '06'=>'Июнь', 
         '07'=>'Июль', '08'=>'Август', '09'=>'Сенябрь', '10'=>'Октябрь', '11'=>'Ноябрь', '12'=>'Декабрь'];
@@ -28,12 +27,12 @@ class RuDate extends Behavior
     {
         $d = $this->owner->$attribute;
         if(!$format)
-           $format = self::$default[Yii::$app->language];
-        if(Yii::$app->language == 'ru-RU') {
-			$format = str_replace('M', mb_substr(self::$month[date('m', $d)], 0, 3), $format);
-			$format = str_replace('F', self::$month[date('m', $d)], $format);
-			return date($format, $d);
-        } else
-            return date($format, $d);
+            $format = self::$default[\Yii::$app->language];
+        if(\Yii::$app->language == 'ru-RU') {
+			$m = self::$month[date('m', $d)];
+            $format = str_replace('M', mb_substr($m, 0, 3, 'UTF-8'), $format);
+            $format = str_replace('F', $m, $format);
+        }
+        return date($format, $d);
     }
 }
